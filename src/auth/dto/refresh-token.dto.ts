2051bb970a291227ea0e1e8314d98f07ce35c53c
Psami-wondah <okechukwusamuel16@gmail.com>
2025-02-05 20:58:21 +0100

    feat: return refresh token

--- src/auth/dto/refresh-token.dto.ts
+++ src/auth/dto/refresh-token.dto.ts
@@ -1,8 +1,9 @@
-import { IsString } from "class-validator";
+import { IsString, IsOptional } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class RefreshTokenDto {
   @ApiProperty()
   @IsString()
+  @IsOptional()
   refreshToken?: string;
 }
